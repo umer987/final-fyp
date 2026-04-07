e3681c1272d861f41184703fd5067af4cd8afecd
syed umer <umershakir987@gmail.com>
2026-04-08 01:17:02 +0500

    adding info for team

--- src/components/Team.tsx
+++ src/components/Team.tsx
@@ -56,7 +56,7 @@ export function Team() {
       image: rameelImage
     }
   ];
-sadfdasdas
+sadfdasdasdas
   const supervisor = {
     name: 'ABDUL WAHAB KHAN',
     title: 'MS, Computer Science',
